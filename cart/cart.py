aeb8c92da808bd2e1ab2f760d74941ce18c0cdff
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-07-21 22:03:47 +0330

    Create some documentation to cart/cart.py

--- cart/cart.py
+++ cart/cart.py
@@ -2,6 +2,9 @@
 
 class Cart:
     def __init__(self, request):
+        """
+        Initialized the Cart
+        """
         self.request = request
         self.session = request.session
         cart = self.session.get('cart')
@@ -13,9 +16,15 @@ class Cart:
         self.cart = cart
 
     def save(self):
+        """
+        Mark Session as Modified to Save Changes
+        """
         self.session.modified = True
 
     def add(self, product, quantity=1):
+        """
+        Add the specified Product to the Cart if it exists
+        """
         product_id = str(product.id)
 
         if product_id not in self.cart:
