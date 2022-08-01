f4d3e028bfa6e21f7ac4da6f99eec0dbac69a272
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-08-01 12:11:34 +0330

    Register Product Model(sample) in products/admin.py & Create 'def __str__(self)' in products/models.py

--- products/models.py
+++ products/models.py
@@ -11,3 +11,6 @@ class Product(models.Model):
 
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
