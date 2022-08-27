4b291a620ffca86cb86f8a424dfc4a97797b2730
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-08-27 13:16:41 +0330

    Modify 'def cart_detail_view()' for Update each Products in cart/views.py

--- cart/views.py
+++ cart/views.py
@@ -8,6 +8,11 @@ from .forms import AddToCartProductForm
 def cart_detail_view(request):
     cart = Cart(request)
 
+    for item in cart:
+        item['product_update_quantity_form'] = AddToCartProductForm(initial={
+            'quantity': item['quantity'],
+            'inplace': True,
+        })
     return render(request, 'cart/cart_detail.html', context={
         'cart': cart,
     })
