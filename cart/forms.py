d0e70087f026485665348948736045961148d45f
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-08-27 12:55:14 +0330

    Modify cart/forms.py

--- cart/forms.py
+++ cart/forms.py
@@ -6,3 +6,5 @@ class AddToCartProductForm(forms.Form):
     QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 31)]
 
     quantity = forms.TypedChoiceField(choices=QUANTITY_CHOICES, coerce=int)
+
+    inplace = forms.BooleanField(required=False, widget=forms.HiddenInput)
