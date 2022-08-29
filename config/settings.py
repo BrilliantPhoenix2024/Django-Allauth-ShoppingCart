347e16b8c07a9a98b993acf8f5af30a3c6869831
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-08-29 15:42:43 +0330

    Modify settings.py to available Cart in all Templates

--- config/settings.py
+++ config/settings.py
@@ -79,6 +79,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # Custom Context Processors
+                'cart.context_processors.cart',
             ],
         },
     },
