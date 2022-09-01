93a05a25c80491d0fe04effffe1a8fda222ef56e
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-09-01 15:47:48 +0330

    Edit 'def __len__()' to sum up the all product's quantities

--- cart/cart.py
+++ cart/cart.py
@@ -67,7 +67,8 @@ class Cart:
             yield item
 
     def __len__(self):
-        return len(self.cart.keys())
+        # return len(self.cart.keys())
+        return sum(item['quantity'] for item in self.cart.values())
 
     def clear(self):
         del self.session['cart']
