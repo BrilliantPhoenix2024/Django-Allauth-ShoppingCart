09e2239210076f0960ffdcaab3ee77c9d07c9aa6
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-09-04 21:25:24 +0330

    ADD 'cover' Filed in 'Product Model' in products/models.py

--- products/models.py
+++ products/models.py
@@ -8,6 +8,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.PositiveIntegerField(default=0)
     active = models.BooleanField(default=True)
+    cover = models.ImageField(verbose_name='Product image', upload_to='product/product_cover/', blank=True)
 
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
